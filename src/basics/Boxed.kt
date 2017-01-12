ad30807f398d451f42ed9a2091c1a030f86e4e2d
mophsic <1084549765@qq.com>
2017-01-12 18:10:11 +0800

    kotlin 类型测试

--- src/basics/Boxed.kt
+++ src/basics/Boxed.kt
@@ -6,9 +6,49 @@ package basics
  */
 
 fun main(args: Array<String>) {
-    val a: Int = 10000
-    println(a == a)
+    val a: Int = 0x0f
+    println(a === a)
     val boxedA: Int? = a
     val anotherBoxedA: Int? = a
     print(boxedA == anotherBoxedA)
+
+    println((a shl 2))
+    println(a.inv())
+
+    println(decimalDigitValue('5'))
+
+    arraytest()
+
+    stringTest()
+}
+
+/**
+ * 将字符转换成[Int]类型
+ */
+fun decimalDigitValue(char: Char): Int {
+    if (char !in '0'..'9') {
+        throw IllegalArgumentException("参数不合法")
+    }
+    return char.toInt() - '0'.toInt()
+}
+
+fun arraytest() {
+    val asc = Array(5, { i -> (i * i).toString() })
+    val x: IntArray = intArrayOf(1, 2, 3, 4, 5)
+
+    //println(x)
+    asc.forEach {
+        i ->
+        println(i)
+    }
+}
+
+fun stringTest() {
+    val text = """
+            >Tell me and I forget.
+            >Teach me and I remember.
+            >Involve me and I learn.""".trimMargin(">")
+    val dollor = "hello"
+    val i: Int = 3
+    println("dollor.length = ${dollor.length}")
 }
